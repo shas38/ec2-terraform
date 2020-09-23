f259f985e021d06f74333f8fad33760269fce3b5
skhan <skhan@staff.compass.edu.au>
2020-09-23 20:33:52 +1000

    define ec2 resourse

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webServer" {
     ami           = "ami-0f158b0f26f18e619" //Ubuntu Server 20.04 LTS
     instance_type = "t2.micro"
-    security_groups = ["${aws_security_group.allow_http.name}"] // Link allow-http security group
+    security_groups = ["${aws_security_group.allow_http.name}"] // Link allow-http security group 
     user_data = <<-EOF
                     #!/bin/bash
                     apt install ruby -y
