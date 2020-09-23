4f8434e4fd9cce4c0d52e9fae877f9e21b656e0f
skhan <skhan@staff.compass.edu.au>
2020-09-23 20:31:09 +1000

    Get the access_key and secret_key from the user

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "webServer" {
+    ami           = "ami-0f158b0f26f18e619" //Ubuntu Server 20.04 LTS
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.allow_http.name}"] // Link allow-http security group
+    user_data = <<-EOF
+                    #!/bin/bash
+                    apt install ruby -y
+                    git clone https://github.com/rea-cruitment/simple-sinatra-app.git
+                    cd simple-sinatra-app/
+                    gem install bundler
+                    bundle install
+                    bundle exec rackup -p 80 --host 0.0.0.0
+                EOF
+    tags = {
+      Name = "Web Server"  
+    } 
+}
---
+++ sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_http" {
+    name        = "allow_http"
+    description = "Allow http traffic"
+
+    // Incoming traffic http only
+    ingress {
+        description = "HTTP"
+        from_port   = 80 
+        to_port     = 80
+        protocol =   "tcp"
+        cidr_blocks =  ["0.0.0.0/0"]
+    }
+    // Outgoing traffic Allow all port all protocol
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_http"
+    }
+}
