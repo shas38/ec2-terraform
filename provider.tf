46ee3f276531a15503652453eef7b370bbe7dfbf
skhan <skhan@staff.compass.edu.au>
2020-09-23 20:34:23 +1000

    set up aws connection

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     access_key = var.access_key 
     secret_key = var.secret_key 
-    region     = "ap-southeast-2" // Sydney Region 
+    region     = "ap-southeast-2" // Sydney Region
 }
